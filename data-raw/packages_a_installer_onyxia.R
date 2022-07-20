d9c33468beadf7338fe74cfc312a962e9a5bf5a8
jean-seb-2106 <jean.sebban@insee.fr>
2022-07-20 10:40:13 +0200

    modifs mineures

--- data-raw/packages_a_installer_onyxia.R
+++ data-raw/packages_a_installer_onyxia.R
@@ -1,3 +1,5 @@
 ## code to prepare `packages_a_installer_onyxia` dataset goes here
 
-usethis::use_data(packages_a_installer_onyxia, overwrite = TRUE)
+install.packages("golem")
+install.packages("shinipsum")
+install.packages("attachment")
--- dev/02_dev.R
+++ dev/02_dev.R
@@ -40,7 +40,7 @@ golem::add_sass_file("custom")
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw(name = "my_dataset", open = FALSE)
+usethis::use_data_raw(name = "packages_a_installer_onyxia", open = FALSE)
 
 ## Tests ----
 ## Add one line by test you want to create
