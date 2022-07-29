09003143329b4bd85e02b24e3cd7a91e5e6aeee3
jean-seb-2106 <jean.sebban@insee.fr>
2022-07-29 11:50:28 +0200

    correction erreur exemple fonction formater_indicateurs1

--- R/fct_formater_indicateurs1.R
+++ R/fct_formater_indicateurs1.R
@@ -2,10 +2,12 @@
 #'
 #' @description Une fonction qui formate les éléments d'un vecteur numérique
 #' @param vec un vecteur numérique
+#' @import dplyr
 #'
 #' @return a vecteur de character
 #' @export
-#' @examples calculer_indicateurs1(mtcars,"mpg") %>% formater_indicateurs1()
+#' @examples a <- calculer_indicateurs1(mtcars,"mpg")
+#' formater_indicateurs1(a)
 formater_indicateurs1 <-function(vec){
   vec %>% round(1) %>% format(big.mark = " ",decimal.mark = ",",nsmall=1)
 }
