a19a5a21c155ac0e534a8b12dfccaadb6a8fc0ea
jean-seb-2106 <jean.sebban@insee.fr>
2022-07-29 11:26:18 +0200

    ajout des deux fonctions pour générer histo et boxplot avec les valeurs extrêmes

---
+++ R/fct_afficher_boxplot2.R
@@ -0,0 +1,21 @@
+#' afficher_boxplot2
+#'
+#' @description Une fonction qui affiche une box plot
+#' à partir d'un vecteur numérique
+#' @param inputselect un character
+#' @param num un vecteur numérique
+#'
+#' @return un graph ggplot
+#' @import ggplot2
+#' @export
+#' @examples b <- ajouter_valeurs_extremes(grandile,"REV_DISPONIBLE",nb=10)
+#' afficher_boxplot2("REV_DISPONIBLE",b)
+afficher_boxplot2 <- function(inputselect,num){
+  c <- num %>% as.data.frame()
+  names(c) <- inputselect
+  ggplot(c) +
+    aes(x = .data[[inputselect]]) +
+    geom_boxplot(col="black",fill="lightgreen") +
+    coord_flip()+
+    theme_minimal()
+}
---
+++ R/fct_afficher_histo2.R
@@ -0,0 +1,20 @@
+#' afficher_histo2
+#'
+#' @description Une fonction qui affiche un histogramme
+#' à partir d'un vecteur numérique
+#' @param inputselect un character
+#' @param num un vecteur numérique
+#'
+#' @return un graph ggplot
+#' @import ggplot2
+#' @export
+#' @examples b <- ajouter_valeurs_extremes(grandile,"REV_DISPONIBLE",nb=10)
+#' afficher_boxplot2("REV_DISPONIBLE",b)
+afficher_histo2 <- function(inputselect,num){
+  c <- num %>% as.data.frame()
+  names(c) <- inputselect
+  ggplot(c) +
+    aes(x = .data[[inputselect]]) +
+    geom_histogram(col="black",fill="lightgreen",bins = 20) +
+    theme_minimal()
+}
